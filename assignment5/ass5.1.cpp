9350b03fd0eacef3e1d0f247f3660685cee7d022
MehakdeepSingh-1024030827 <msingh1_be24@thapar.edu>
2025-10-11 14:24:58 +0530

    Update ass5.1.cpp

--- assignment5/ass5.1.cpp
+++ assignment5/ass5.1.cpp
@@ -185,41 +185,74 @@ int main(){
     newnode->Print(newnode);
     Node* tail = newnode;
     int choice;
-    cout<<"enter which type of operation you want to perform : ";
+    cout<<"enter which type of operation you want to perform : "<<endl;
+    cout<<"For Inserting a data value at beginning ENTER 1 : "<<endl;
+    cout<<"For Inserting a data value at last ENTER 2 : "<<endl;
+    cout<<"For Inserting a data value before any other ENTER 3 : "<<endl;
+    cout<<"For Inserting a data value after any other ENTER 4 : "<<endl;
+    cout<<"For Deleting a data value from beginning ENTER 5 : "<<endl;
+    cout<<"For Deleting a data value from last ENTER 6 : "<<endl;
+    cout<<"For Deleting a specific data value ENTER 7 : "<<endl;
+    cout<<"For searching a value ENTER 8 : "<<endl;
     cin>>choice;
     switch(choice){
 
         case 1 : 
-        newnode->InsertAtBeginning(newnode, 60);
+        int x1;
+        cout<<"enter value to insert : ";
+        cin>>x1;
+        newnode->InsertAtBeginning(newnode, x1);
         newnode->Print(newnode);
         break;
 
         case 2:
-        newnode->InsertAtLast(tail,70);
+        int x2;
+        cout<<"enter value to insert : ";
+        cin>>x2;
+        newnode->InsertAtLast(tail,x2);
         newnode->Print(newnode);
 
         case 3:
-        newnode->InsertBefore(newnode,20);
+        int x3;
+        cout<<"enter data value before which it is to be inserted : ";
+        cin>>x3;
+        newnode->InsertBefore(newnode,x3);
         newnode->Print(newnode);
 
         case 4:
-        newnode->InsertAfter(newnode,10);
+        int x4;
+        cout<<"enter data value after which it is to be inserted : ";
+        cin>>x4;
+        newnode->InsertAfter(newnode,x4);
         newnode->Print(newnode);
 
         case 5:
-        newnode->DeleteFromBeginning(newnode,3);
+        int x5;
+        cout<<"enter postion from starting where element is to be deleted : ";
+        cin>>x5;
+        newnode->DeleteFromBeginning(newnode,x5);
         newnode->Print(newnode);
         
         case 6 :
-        newnode->DeleteFromEnd(newnode,2);
+        int x6;
+        cout<<"enter postion from last where element is to be deleted : ";
+        cin>>x6;
+        newnode->DeleteFromEnd(newnode,x6);
         newnode->Print(newnode);
 
         case 7 :
-        newnode->DeleteSpecific(newnode,40);
+        int x7;
+        cout<<"enter element which is to be deleted : ";
+        cin>>x7;
+        newnode->DeleteSpecific(newnode,x7);
         newnode->Print(newnode);
 
         case 8 :
-        newnode->Search(newnode, 20);
+        int x8;
+        cout<<"enter element which is to be searched : ";
+        cin>>x8;
+        newnode->Print(newnode);
+        newnode->Search(newnode, x8);
     }
 
 
